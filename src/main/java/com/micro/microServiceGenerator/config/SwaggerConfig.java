2904959b477a156934ef51fb16fbcccd94ca8ea3
Pavan Saragadam <PavanS@skytechsolutions.com>
2018-12-27 17:03:27 +0530

    changes for doc

--- src/main/java/com/micro/microServiceGenerator/config/SwaggerConfig.java
+++ src/main/java/com/micro/microServiceGenerator/config/SwaggerConfig.java
@@ -1,8 +1,12 @@
 package com.micro.microServiceGenerator.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -15,10 +19,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
+          .apis(RequestHandlerSelectors.any())
           .paths(PathSelectors.any())                          
           .build().enable(true);                                           
     }
+    
+    private ApiInfo apiInfo() {
+		return new ApiInfo("MicroServiceGenerator",
+				"This application generates Spring boot micro services automatically.(BETA version)",
+				"1.0.0", "TERMS OF SERVICE URL",
+				new Contact("Pavan - For Support", " www.linkedin.com/in/pavansaragadam ", "pavan.saragadam@gmail.com"),
+				"", "", Collections.emptyList()) ;
+	}
 }
