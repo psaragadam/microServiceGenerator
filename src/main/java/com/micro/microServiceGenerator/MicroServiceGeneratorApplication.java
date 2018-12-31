39560de5f96dab09d6ac7de394f19bb349a5c0f3
pavan.saragadam@gmail.com <pavan.saragadam@gmail.com>
2018-12-31 20:26:33 +0530

    war file changes

--- src/main/java/com/micro/microServiceGenerator/MicroServiceGeneratorApplication.java
+++ src/main/java/com/micro/microServiceGenerator/MicroServiceGeneratorApplication.java
@@ -1,12 +1,41 @@
 package com.micro.microServiceGenerator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MicroServiceGeneratorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MicroServiceGeneratorApplication.class, args);
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class MicroServiceGeneratorApplication  extends SpringBootServletInitializer{
+    
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MicroServiceGeneratorApplication.class);
 	}
+
+	
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceGeneratorApplication.class, args);
+    }
+    
+    /*@Bean
+    public ErrorPageFilter errorPageFilter() {
+        return new ErrorPageFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.setEnabled(false);
+        return filterRegistrationBean;
+    }
+*/
 }
